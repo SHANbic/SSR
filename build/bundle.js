b3f7937beccceb0ec9c28dfc4c1786bc6a7bab8c
Pierre <pierre.lam91@gmail.com>
2019-08-02 14:20:14 +0200

    [ FEATURE ] Now serving html along with scripts

--- build/bundle.js
+++ build/bundle.js
@@ -7053,9 +7053,12 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var app = (0, _express2.default)();
 var port = 4000;
 
+app.use(_express2.default.static('public'));
+
 app.get('/', function (req, res) {
   var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
-  res.send(content);
+  var html = '\n  <html>\n    <head></head>\n    <body> \n      <div>' + content + '</div>\n      <script src="bundle.js"></script>\n    </body>\n  </html>\n ';
+  res.send(html);
 });
 
 app.listen(port, function () {
