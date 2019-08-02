5738cd8b6d7a7d570440f767152429c57715da71
Pierre <pierre.lam91@gmail.com>
2019-08-02 23:45:21 +0200

    [ FIX ] Fixed errors in both action and reducer

--- src/client/reducers/usersReducer.js
+++ src/client/reducers/usersReducer.js
@@ -3,7 +3,7 @@ import { FETCH_USERS } from '../actions';
 export default (state = [], action) => {
   switch (action.type) {
     case FETCH_USERS:
-      return payload.data;
+      return action.payload.data;
     default:
       return state;
   }
