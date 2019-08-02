ae50f17bdb98be8771edc26db54ffc2fb1fd1d1d
Pierre <pierre.lam91@gmail.com>
2019-08-02 23:12:40 +0200

    [ FEATURE ] Created a users list component to render users

---
+++ src/client/components/UsersList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchUsers } from '../actions';
+
+class UsersList extends Component {
+  componentDidMount() {
+    //this.props.fetchUsers();
+  }
+
+  renderUsers() {
+    this.props.users.map(user => {
+      <li key={user.id}>{user.name}</li>;
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        list of users
+        <ul>{this.renderUsers()}</ul>
+      </div>
+    );
+  }
+}
+function mapStateToProps(state) {
+  return { users: state.users };
+}
+
+export default connect(
+  mapStateToProps,
+  { fetchUsers }
+)(UsersList);
