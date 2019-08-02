2721dee904e7244748743a8afff2e5d535d12f80
Pierre <pierre.lam91@gmail.com>
2019-08-02 23:44:45 +0200

    [ FEATURE ] Updated users list to actually render fetched users

--- src/client/components/UsersList.js
+++ src/client/components/UsersList.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { fetchUsers } from '../actions';
 
-class UsersList extends Component {
+class UsersList extends React.Component {
   componentDidMount() {
-    //this.props.fetchUsers();
+    this.props.fetchUsers();
   }
 
   renderUsers() {
-    this.props.users.map(user => {
-      <li key={user.id}>{user.name}</li>;
+    return this.props.users.map(user => {
+      return <li key={user.id}>{user.name}</li>;
     });
   }
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill';
 import express from 'express';
 import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
@@ -10,8 +11,6 @@ app.use(express.static('public'));
 app.get('*', (req, res) => {
   const store = createStore();
 
-
-  
   res.send(renderer(req, store));
 });
 
