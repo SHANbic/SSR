cfe2b353bde8c80e895d79f14b406c3cdc368127
Pierre <pierre.lam91@gmail.com>
2019-08-03 18:00:02 +0200

    [ CONFIG ] Now able to match Routes with Components needed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import 'babel-polyfill';
 import express from 'express';
+import { matchRoutes } from 'react-router-config';
+import Routes from './client/Routes';
 import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
 
 const app = express();
-const port = 4000;
+const port = 3000;
 
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
   const store = createStore();
-
+  console.log(matchRoutes(Routes, req.path));
   res.send(renderer(req, store));
 });
 
