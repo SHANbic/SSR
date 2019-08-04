5fddaf8627d0ab69d8d20f25779b3c1feb768664
Pierre <pierre.lam91@gmail.com>
2019-08-04 09:45:06 +0200

    [ REFACTO ] Now exporting components as objects

--- src/client/pages/UsersListPage.js
+++ src/client/pages/UsersListPage.js
@@ -27,12 +27,13 @@ function mapStateToProps(state) {
 }
 
 function loadData(store) {
- return  store.dispatch(fetchUsers());
+  return store.dispatch(fetchUsers());
 }
 
-export { loadData };
-
-export default connect(
-  mapStateToProps,
-  { fetchUsers }
-)(UsersList);
+export default {
+  loadData,
+  component: connect(
+    mapStateToProps,
+    { fetchUsers }
+  )(UsersList)
+};
